f2716e477bd0b2d6f94088b06936eb5c6f850c83
Abdelmadjid-boudiaf-16 <boudiafabdelmadjid16@gmail.com>
2024-10-06 09:25:38 +0100

    add callToAction  & footer sections

--- app/sections/CallToAction.tsx
+++ app/sections/CallToAction.tsx
@@ -1,3 +1,35 @@
+import MagicButton from "@/components/button";
+
+import gridLines from '@/assets/grid-lines.png'
+import starsBg from '@/assets/stars.png'
+
 export const CallToAction = () => {
-  return <section>CallToAction</section>;
+  return (
+    <section className="py-20 md:py-24">
+      <div className="container">
+        <div
+          className="relative overflow-hidden rounded-xl border border-white/20 py-20 md:py-24"
+          style={{
+            backgroundImage: `url(${starsBg.src})`,
+          }}
+        >
+          <div
+            className="absolute inset-0 bg-[#4A208A] bg-blend-overlay [mask-image:radial-gradient(50%_50%_at_50%_35%,black,transparent)]"
+            style={{ backgroundImage: `url(${gridLines.src})` }}
+          ></div>
+          <div className="relative">
+            <h2 className="text-center text-5xl font-medium tracking-[-1px] md:text-6xl">
+              AI-driven SEO for everyone.
+            </h2>
+            <p className="mx-auto max-w-[320px] px-4 pt-5 text-center text-lg leading-7 text-white/70 md:text-xl">
+              Achieve clear, impactful results without the complexity.
+            </p>
+            <div className="mx-auto w-fit py-5">
+              <MagicButton text="Join waitlist" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
--- app/sections/Footer.tsx
+++ app/sections/Footer.tsx
@@ -1,3 +1,88 @@
+import LogoIcon from "@/assets/logo.svg";
+import XLogo from "@/assets/social-x.svg";
+import InstagramLogo from "@/assets/social-instagram.svg";
+import YoutubeLogo from "@/assets/social-youtube.svg";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+const linkList = [
+  {
+    link: "features",
+    path: "/",
+  },
+  {
+    link: "developers",
+    path: "/",
+  },
+  {
+    link: "company",
+    path: "/",
+  },
+  {
+    link: "Blog",
+    path: "/",
+  },
+  {
+    link: "changelog",
+    path: "/",
+  },
+];
 export const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="py-5 border-t border-white/15">
+      <div className="container">
+        <div className="flex w-[320px] flex-col px-5 lg:w-full lg:flex-row lg:items-center lg:justify-between">
+          <div className="flex items-center space-x-2">
+            <LogoIcon className="h-6 w-6" />
+            <h1 className="text-sm">AI Startup Landing Page</h1>
+          </div>
+          <ul className="flex flex-col gap-5 pt-8 lg:flex-row lg:items-center lg:pt-0">
+            {linkList.map((item, idx) => (
+              <li key={idx} className="list-none">
+                <Link
+                  href={item.path}
+                  className={cn(
+                    "text-xs text-white/70 transition-colors duration-300 hover:text-white md:text-sm",
+                  )}
+                >
+                  {item.link}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <ul className="flex items-center space-x-5 pt-8 lg:pt-0">
+            <li className="list-none">
+              <Link
+                href={"/"}
+                className={cn(
+                  "text-white/40 transition-colors duration-300 hover:text-white",
+                )}
+              >
+                <XLogo className="h-6 w-6" />
+              </Link>
+            </li>
+            <li className="list-none">
+              <Link
+                href={"/"}
+                className={cn(
+                  "text-white/40 transition-colors duration-300 hover:text-white",
+                )}
+              >
+                <InstagramLogo className="h-6 w-6" />
+              </Link>
+            </li>
+            <li className="list-none">
+              <Link
+                href={"/"}
+                className={cn(
+                  "text-white/40 transition-colors duration-300 hover:text-white",
+                )}
+              >
+                <YoutubeLogo className="h-6 w-6" />
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 };
